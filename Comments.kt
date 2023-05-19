06f473b125b5f0c4bdb8cb5fc4abd4d0f938bc9d
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-05-19 19:34:37 +0300

    Create Comments.kt

---
+++ Comments.kt
@@ -0,0 +1,61 @@
+
+
+
+In C#, comments are used to add explanatory notes or documentation within the code.
+Comments are ignored by the compiler and have no impact on the execution of the program.
+They serve as a way to improve code readability and provide additional information for developers or future maintainers of the code.
+Heres an explanation of the different types of comments in C# with code samples:
+
+Single-line Comments:
+Single-line comments start with // and continue until the end of the line. They are used for brief comments or explanations on a single line.
+csharp
+Copy code
+int x = 5;  // This is a single-line comment
+Multi-line Comments:
+Multi-line comments start with /* and end with */. They can span multiple lines and are used for longer comments or block explanations.
+csharp
+Copy code
+/*
+This is a multi-line comment.
+It can span multiple lines.
+*/
+
+int x = 5;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+...
