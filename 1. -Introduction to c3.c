99647ef81b71fe0ec81ee5048a90586b646f8902
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-05-19 14:15:06 +0300

    Update 1. -Introduction to c3.c

--- 1. -Introduction to c3.c
+++ 1. -Introduction to c3.c
@@ -3,15 +3,29 @@
 c# vs .NET
 
 C# is a programinnign language
-.net is a framwoerk for building applications on windows, .Net is not limited to only C# as there are other languages that can can be used to build   appications on top of .net
+.net is a framwoerk for building applications on windows, .Net is not limited to only C# as there are other languages that can can be used
+to build   appications on top of .net, e.g f# and v.net
+
+
+.NET FRAMWORK
+It is comprsed of the folloowing
+CLR (Common  Language Runtime)  - c# shap is platform independednt becuse it is first transformed to Itermindie Langiage code(IL code), before it is transformed into native machine code, CLR covertes this Intermdeiaete Language Code into the native machib=ne code threouc  aprocess called Just In Time compilation or JiT
+Class Libray  - For building applications
 
 
 
 
+ARCHITECTURE OF .NET APPLICATIONS
 
 
+APPLICATION - On a high level, your application consit of building claseese. This classes work together during runtime to provide some functionality
 
 
+Class is a container which has data(also called attribyted) and functions (also caleed methods)
+  In real world applications there are lots of classes with different functionaliyt, e.g get data from the user, process the data , and so on
+  As the number of classes grow in our application, we need a way to organise these clases, That is where Namespace comes in
+  
+  NAMESPACE - IS A CONTAINER FOR RELATED CLASESS
 
 
 
