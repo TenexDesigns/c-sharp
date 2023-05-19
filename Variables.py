a84eb272d3820091ef72afb5436450be4d119af0
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-05-19 16:28:55 +0300

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,91 @@
+
+Variables and Constants
+
+
+Variable : - A name give to a storage location in memeory
+Contsnt : - An immutable value  
+
+
+DECLARING VARAIBLES
+
+int number;
+int Number =1
+
+The first and last are not the same , as c# is a case sensitive langiage, so the first and last are not the same,
+We can not use a varialbe that is not inistailzed
+
+DECLARING CONSTABNTS
+We have to intialize a constant and set its value
+
+const float Pi = 3.14f
+
+
+
+NAMING CONVENTIONS
+camel case = firstName
+pascal Case = FirstName
+Hungarialn Notation = strFirstName  --> Not used in C#
+
+
+
+for local variable : Camel case ----- > int number;
+for constants  :Pascal case     ----- > constant int MaxZom = 5;        
+
+
+primitive types in c#
+
+
+In C#, primitive types are the basic data types provided by the language to represent simple values. These types are built into the C# language and are used to store common types of data. Here are the primitive types in C#:
+
+Boolean: Represents a Boolean value that can be either true or false.
+
+Byte: Represents an 8-bit unsigned integer value.
+
+SByte: Represents an 8-bit signed integer value.
+
+Char: Represents a single Unicode character.
+
+Decimal: Represents a decimal floating-point number with higher precision than float or double.
+
+Double: Represents a double-precision floating-point number.
+
+Float: Represents a single-precision floating-point number.
+
+Int: Represents a 32-bit signed integer value.
+
+UInt: Represents a 32-bit unsigned integer value.
+
+Long: Represents a 64-bit signed integer value.
+
+ULong: Represents a 64-bit unsigned integer value.
+
+Short: Represents a 16-bit signed integer value.
+
+UShort: Represents a 16-bit unsigned integer value.
+
+IntPtr: Represents a signed pointer or a signed integer that is the same size as a pointer. Used for interoperability with unmanaged code.
+
+UIntPtr: Represents an unsigned pointer or an unsigned integer that is the same size as a pointer. Used for interoperability with unmanaged code.
+
+These primitive types provide a way to store and manipulate basic data in C#. Each type has its own range of values and memory requirements.
+Additionally, C# also provides non-primitive types such as strings, arrays, classes, and structures, which are built using these primitive types.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
