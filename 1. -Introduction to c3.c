91ef05a7eccac05cdb77729d989985847f2c78e4
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-05-19 13:58:57 +0300

    Create 1. -Introduction to c3.c

---
+++ 1. -Introduction to c3.c
@@ -0,0 +1,38 @@
+
+
+c# vs .NET
+
+C# is a programinnign language
+.net is a framwoerk for building applications on windows, .Net is not limited to only C# as there are other languages that can can be used to build   appications on top of .net
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
